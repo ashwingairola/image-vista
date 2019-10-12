6f0a180589c3e3848716c9d672a772dbaf033458
Ashwin Gairola <ashwingairola999@gmail.com>
2019-10-13 00:18:46 +0530

    Created a rudimentary CSS grid.

--- src/app/routes/search/components/image-results-grid/image-results-grid.component.ts
+++ src/app/routes/search/components/image-results-grid/image-results-grid.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { Unsplash } from '@imgvista/contracts/unsplash.contract';
+
 @Component({
   selector: 'app-image-results-grid',
   templateUrl: './image-results-grid.component.html',
   styleUrls: ['./image-results-grid.component.scss']
 })
 export class ImageResultsGridComponent implements OnInit {
-  @Input() searchResult: any;
+  @Input() searchResult: Unsplash.ISearchResult;
 
   constructor() {}
 
